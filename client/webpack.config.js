cb67f7ebcc065e201b3e854703525107ebf78de2
Balaji Rajendran <karurbalamathi@gmail.com>
2019-08-08 21:29:52 +0530

    React Project Updated

---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+			watchContentBase: true,
+			proxy: {
+				'/backend/*': {
+					target: 'http://localhost:5000/',
+					secure: false,
+				}
+			},
+		},
+	};
+}
